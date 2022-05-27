e39a7fbc4930fd358f5e62ad799fb33323cae8fb
Sagat <sagatfarhat@gmail.com>
2022-05-28 00:01:54 +0600

    what should I change if everything ok

--- src/com/company/Vertex.java
+++ src/com/company/Vertex.java
@@ -2,9 +2,17 @@ package com.company;
 import java.util.ArrayList;
 
 public class Vertex <T> {
-
+    private Vertex node;
     int index;
     ArrayList<Vertex> adjacent;
+
+    public void setNode(Vertex node){
+        this.node=node;
+    }
+
+    public Vertex getNode(){
+        return node;
+    }
 
     public Vertex(int index) {
         adjacent = new ArrayList<>();
@@ -18,5 +26,9 @@ public class Vertex <T> {
 
     public void put(Vertex vertex){
         adjacent.add(vertex);
+    }
+
+    public int size(){
+        return adjacent.size();
     }
 }
